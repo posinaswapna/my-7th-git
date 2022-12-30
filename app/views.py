4d0ca41c41414b2a60281db75dd03b113f14c873
posinaswapna <posinaswapna@gmail.com git config --global user.name posinaswapna>
2022-12-30 15:45:22 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def yash(request):
+    return render(request,'kgf.html')
+
+def yash1(request):
+    return render(request,'kgf2.html')   
